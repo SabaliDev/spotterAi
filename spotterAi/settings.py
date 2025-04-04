ec45fba2bee4b35cf95fdd4aadd86e51fbcb9304
SabaliDev <sabali@nathandigital.com>
2025-04-04 13:26:38 +0300

    changed py version

--- spotterAi/settings.py
+++ spotterAi/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'core',
-    'authentication',
+    'authentication.apps.AuthenticationConfig',
     'tracking',
     'routing',
     'compliance',
